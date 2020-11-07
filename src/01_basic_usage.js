a2227a08983104011f33e4c2b3b6e660987f9040
Ther <403240503@qq.com>
2020-11-07 20:22:24 +0800

    feat: add snabbdom

---
+++ src/01_basic_usage.js
@@ -0,0 +1,13 @@
+// // var snabbdom = require('snabbdom');
+// 导入 snabbdom
+// import snabbdom from 'snabbdom'
+
+// console.log(snabbdom)
+
+import { h } from 'snabbdom/build/package/h';
+import { init } from 'snabbdom/build/package/init';
+import { classModule } from 'snabbdom/build/package/modules/class';
+
+console.log(h)
+console.log(init)
+console.log(classModule)
